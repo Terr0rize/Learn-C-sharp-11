f56c15e0fd99cda3505bb3474d05165e754fa8f5
kitsune <57214464+Terr0rize@users.noreply.github.com>
2020-09-03 15:43:14 +0400

    Add files via upload

---
+++ C sharp learning/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic; //массив, списки.
+using System.ComponentModel;
+using System.Globalization;
+using System.Net;
+
+namespace C_sharp_learning
+{
+    class Animal
+   {
+       public string Name { get; set; }
+       public Animal(string name)
+        {
+            Name = name;
+        }
+        public void Print()
+        {
+            Console.WriteLine(Name);
+        }
+    }
+
+    class Dog :  Animal
+    {
+        private float speed;
+       public Dog(float speed, string name) : base(name)
+        {
+            this.speed = speed;
+            Console.WriteLine("Speed: " + speed);
+          
+        }
+    }
+    class MainClass
+    {
+        static void Main(string[] args) //метод срабатывает при старте программы
+        {
+            Dog tom = new Dog(12.23f, "Tom" );
+            Console.WriteLine(tom.Name);
+
+            Dog jerry = new Dog(23.25f, "Jerry");
+            jerry.Name = "Jerry";
+           
+            Console.WriteLine();
+            Console.WriteLine(jerry.Name);
+            jerry.Print();
+        }
+    }
+
+
+}
